43759bb38131455ca947901d74ddc730ff91b80e
Mohamed Yasser <96927790+MohamedYasser343@users.noreply.github.com>
2023-02-09 19:30:47 -0500

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS todo_item;
+DROP TABLE IF EXISTS todo_list;
+
+CREATE TABLE todo_list (
+    id serial PRIMARY KEY,
+    title varchar(150) NOT NULL
+);
+
+CREATE TABLE todo_item (
+    id serial PRIMARY KEY,
+    title varchar(150) NOT NULL,
+    checked boolean NOT NULL DEFAULT false,
+    list_id integer NOT NULL,
+    FOREIGN KEY (list_id) REFERENCES todo_list(id)
+);
+
+INSERT INTO todo_list (title) VALUES ('List 1'), ('List 2');
+INSERT INTO todo_item (title, list_id)
+    VALUES ('Item 1', 1), ('Item 2', 2), ('Item 1', 2);
